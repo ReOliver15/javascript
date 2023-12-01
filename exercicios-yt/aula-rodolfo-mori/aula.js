6a92106a477f973bb824405b43b7a02ea3375d19
ReOliver15 <vick.oliveira215@gmail.com>
2023-12-01 17:41:10 -0300

    aula js

---
+++ exercicios-yt/aula-rodolfo-mori/aula.js
@@ -0,0 +1,14 @@
+/*
+    Variáveis
+     - Let / Const / Var (Não é boa prática utilizar pois declara variáveis globais)
+
+     const - valor constante que não muda ao longo do tempo
+     let - posso alterar o valor sempre que precisar
+*/
+
+let numero = 10
+
+numero= 20 // A variável numero vai receber o valor 20
+
+console.log(numero) //console.log imprime algo na tela, também disponivel no devtools
+
